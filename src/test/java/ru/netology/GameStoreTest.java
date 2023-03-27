fb277ed0192ae71718dee8934446d4713c60a505
Neto Homework <clychnik@gmail.com>
2023-03-27 22:41:04 +0300

    Test for issues #1 and #2

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,18 +1,47 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
+    GameStore store = new GameStore();
 
     @Test
     public void shouldAddGame() {
-
-        GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
 
         assertTrue(store.containsGame(game));
     }
 
-    // другие ваши тесты
+    @Test
+    void addPlayTime() {
+        store.addPlayTime("Player1", 100);
+        store.addPlayTime("Player1", 200);
+        store.addPlayTime("Player1", 300);
+
+//        assertEquals(100 + 200 + 300, store.getTimePLayed("Player1"));
+    }
+
+    @Test
+    void getMostPlayer() {
+        store.addPlayTime("Player1", 100);
+        store.addPlayTime("Player2", 200);
+        store.addPlayTime("Player3", 300);
+
+        Assertions.assertEquals("Player3", store.getMostPlayer());
+    }
+
+    @Test
+    void getSumPlayedTime() {
+        store.addPlayTime("Player1", 100);
+        store.addPlayTime("Player2", 200);
+        store.addPlayTime("Player3", 300);
+        Assertions.assertEquals(300 + 200 + 100, store.getSumPlayedTime());
+    }
+
+    @Test
+    void containsGame() {
+    }
 }
